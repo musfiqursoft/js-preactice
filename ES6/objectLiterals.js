f71e4fd039db4e39fd8f9d699bafd18a578705e2
musfiqursoft <mostafizurfiverr90@gmail.com>
2021-09-09 15:21:36 +0600

    obiect litetral added

---
+++ ES6/objectLiterals.js
@@ -0,0 +1,21 @@
+function student1(name, age) {
+    return {
+        name: name,
+        age: age
+    }
+}
+
+console.log(student1("Musfiqur Rahman Rimon", 17));
+
+
+// function student2(name, id) {
+//     return {
+//         name: name,
+//         id: id
+//     }
+// }
+
+// var result = student1("Rimon ", 1232);
+// console.log(result);
+
+

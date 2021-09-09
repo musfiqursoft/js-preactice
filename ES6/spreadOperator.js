7980929f106e5b427e99aaaf5077b536c58d3b68
musfiqursoft <mostafizurfiverr90@gmail.com>
2021-09-09 13:57:54 +0600

    es 6 is updated

--- ES6/spreadOperator.js
+++ ES6/spreadOperator.js
@@ -1,6 +1,11 @@
 function addNumbers(x, y, z) {
-    return x+ y+ z;
+    return x + y + z;
 }
 let numbers = [1, 2, 3];
 
-console.log(addNumbers(...numbers) );
+// console.log(addNumbers(...numbers));
+
+
+
+let numbers1 = [4, 5, 6, ...numbers];
+console.log(numbers1);

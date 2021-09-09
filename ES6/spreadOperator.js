eae7a17531c034ae4bfd17c6dea8de39d32a7f17
musfiqursoft <mostafizurfiverr90@gmail.com>
2021-09-09 14:13:12 +0600

    object literals addded

--- ES6/spreadOperator.js
+++ ES6/spreadOperator.js
@@ -8,4 +8,12 @@ let numbers = [1, 2, 3];
 
 
 let numbers1 = [4, 5, 6, ...numbers];
-console.log(numbers1);
+// console.log(numbers1);
+
+
+let nums1 = [1, 2, 3];
+let nums2 = [4, 5, 6];
+// let num = nums1.concat(nums2);
+let num = [...nums1, ...nums2];
+console.log(num);
+

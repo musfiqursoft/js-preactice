0d7aa39e5300457df552b26c9392059325a13db0
musfiqursoft <mostafizurfiverr90@gmail.com>
2021-09-10 21:27:24 +0600

    map, filter function

--- ES6/map, filter function.js
+++ ES6/map, filter function.js
@@ -1,5 +1,12 @@
-let numbers = [2, 3, 4, 5];
-let squreNumbers = numbers.map(function (x) {
-    return x * x;
+// let numbers = [2, 3, 4, 5];
+// let squreNumbers = numbers.map(function (x) {
+//     return x * x;
+// })
+// console.log(squreNumbers);
+
+// filter fution
+let numbers = [3, 5, 7, 12, 8, 20, 60];
+let newNumbers = numbers.filter(function (x) {
+    return x > 10;
 })
-console.log(squreNumbers);
+console.log(newNumbers);
